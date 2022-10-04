ce8ad4c0083b18c6ee0e1f2eef248fc2e7ab0fa8
Júlio Bengtson Neves <30966981+J-Bengtson@users.noreply.github.com>
2022-10-04 19:49:36 +0000

    feat: implement Auth, User, Payment, and Notification services with Docker support and RabbitMQ integration

--- NotificationService/Program.cs
+++ NotificationService/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddMassTransit(x =>
 
     x.UsingRabbitMq((context, cfg) =>
     {
-        cfg.Host("localhost", "/", h =>
+        cfg.Host(builder.Configuration["RabbitMQ:Host"] ?? "localhost", "/", h =>
         {
             h.Username("guest");
             h.Password("guest");
